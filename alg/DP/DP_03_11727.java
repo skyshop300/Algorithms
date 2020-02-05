ae4ce0add032b97371cc3d2890f96909b84a3dbc
skyshop300 <skyshop5210@gmail.com>
2020-02-05 19:55:09 +0900

    dp_04_9095

---
+++ alg/DP/DP_03_11727.java
@@ -0,0 +1,20 @@
+package alg.DP;
+import java.util.*;
+
+public class DP_03_11727 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] d = new int[n + 1];
+
+		d[1] = 1;
+		if(n >= 2) {
+			d[2] = 3;
+		}
+		for(int i = 3; i <= n; i++) {
+			d[i] = d[i-1] + d[i-2] * 2;		// 핵심
+			d[i] %= 10007;
+		}
+		System.out.println(d[n]);
+	}
+}
---
+++ alg/DP/dp_02_11726.java
@@ -0,0 +1,23 @@
+// 백준11726: 2×n 타일링
+package alg.DP;
+import java.util.*;
+
+public class dp_02_11726 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] d = new int[n + 1];		// 2*n 직사각형을 채우는 방법의 수
+
+		d[0] = 0;
+		d[1] = 1;
+		if(n >= 2) {
+			d[2] = 2;
+		}
+
+		for(int i = 3; i <= n; i++) {
+			d[i] = d[i - 1] + d[i - 2];
+			d[i] %= 10007;
+		}
+		System.out.println(d[n]);
+	}
+}
