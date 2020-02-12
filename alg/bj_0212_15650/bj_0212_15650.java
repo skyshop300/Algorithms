98ca78d6373661ca0afb79a24b87f79edf832059
skyshop300 <skyshop5210@gmail.com>
2020-02-12 21:39:25 +0900

    bj_15650

--- alg/bj_0212_15650/bj_0212_15650.java
+++ alg/bj_0212_15650/bj_0212_15650.java
@@ -10,12 +10,13 @@ public class bj_0212_15650 {
 		int depth = 0;
 		boolean[] visit = new boolean[n+1];
 		int[] temp = new int[m+1];
+		
+		temp[0] = 1;
 
 		nm(temp, visit, depth, n, m);
 	}
 
 	public static void nm(int[] temp, boolean[] visit, int depth, int n, int m) {
-		depth++;
 		if (depth == m) {
 			for (int i = 1; i < m + 1; i++) {
 				System.out.print(temp[i] + " ");
@@ -23,16 +24,14 @@ public class bj_0212_15650 {
 			System.out.println();
 			return;
 		}
-		
-		for(int i = 1; i < n+1; i++) {
+
+		for(int i = temp[depth]; i < n+1; i++) {
 			if(!visit[i]) {
 				visit[i] = true;
-				temp[depth] = i;
-				nm(temp, visit, depth, n, m);
+				temp[depth + 1] = i;
+				nm(temp, visit, depth + 1, n, m);
 			}
 			visit[i] = false;
 		}
-
-	
 	}
 }
