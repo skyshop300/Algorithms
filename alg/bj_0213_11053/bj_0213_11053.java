85394ac339ff703304c62035205e5de52197c77e
skyshop300 <skyshop5210@gmail.com>
2020-02-14 17:39:06 +0900

    bj_11053_ING

--- alg/bj_0213_11053/bj_0213_11053.java
+++ alg/bj_0213_11053/bj_0213_11053.java
@@ -8,19 +8,34 @@ public class bj_0213_11053 {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int[] a = new int[n+1];
-		int[] dp = new int[n+1];
-		int maxDp = 0;
-		for(int i = 1 ; i < n+1; i++) {
-			a[i] = sc.nextInt();
-			for(int j = 0; j < i + 1; j++) {
-				if(a[i] > a[j]) {
-					dp[i] = dp[j] + 1;
-				} else {
+		int[] dp = new int[n+1];	// 수열 갯수
 
-				}
+		dp[1] = 1;
+
+		for(int i = 1 ; i < n + 1; i++) {
+			a[i] = sc.nextInt();
+			int max = 0;
+			int maxDp = 0;
+			for(int j = 0; j < i; j++) {
+				System.out.println("j" + j);
+				System.out.println("a[i]" + a[i]);
+				System.out.println("a[j]" + a[j]);
+				System.out.println("max" + max);
+				if(a[i] > a[j] && max < a[j] ) {
+					max = a[j];
+					dp[i] = dp[j] + 1;
+					System.out.println("j: " + j);
+					System.out.println("dp[j]: " + dp[j]);
+					System.out.println("i: " + i);
+					System.out.println("dp[i]: " + dp[i]);
+					System.out.println();
+				} 
 			}
 		}
 		Arrays.sort(dp);
 		System.out.println(dp[dp.length - 1]);
 	}
 }
+
+/* 7 
+1 4 5 2 3 4 5 */
