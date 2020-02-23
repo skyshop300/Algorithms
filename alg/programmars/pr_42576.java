4ba76771d0b4771304d4165e16cc3dfa00f540c6
skyshop300 <skyshop5210@gmail.com>
2020-02-23 22:06:59 +0900

    pr_42576

---
+++ alg/programmars/pr_42576.java
@@ -0,0 +1,24 @@
+package alg.programmars;
+import java.util.*;
+public class pr_42576 {
+    public static void main(String[] args) {
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"eden", "kiki"};
+        System.out.println(solution(participant, completion));
+    }
+        
+
+    public static String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        String answer = participant[participant.length - 1];
+        
+        for(int i = 0; i < completion.length; i++) {
+            if(!participant[i].equals(completion[i])) {
+                answer = participant[i];
+                break;
+            }
+        }
+        return answer;
+    }
+}
