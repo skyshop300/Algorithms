2e53da6b2cda305bc95273781110fea963a7f6e8
skyshop300 <skyshop5210@gmail.com>
2020-03-09 20:56:48 +0900

    pr_43237

--- alg/programmars/pr_0308_43237_BK/pr_43237.java
+++ alg/programmars/pr_0308_43237_BK/pr_43237.java
@@ -1,9 +1,9 @@
-package alg.programmars.pr_0308_43237;
+package alg.programmars.pr_0308_43237_BK;
 import java.util.*;
 
 public class pr_43237 {
 	public static void main(String[] args) {
-		int[] budgets = {0, 42, 43, 42, 42, 41, 44};
+		int[] budgets = {1, 100, 105, 55, 42, 41, 44};
 		int M = 300;
 		Solution sol = new Solution();
 		System.out.println(sol.solution(budgets, M));
@@ -26,16 +26,20 @@ class Solution {
 
 		// 369 11
 
-		// Arrays.sort(budgets);
-		// int maxBudget = budgets[budgets.length - 1];
+		Arrays.sort(budgets);
+		int maxBudget = budgets[budgets.length - 1];
+		answer = (int)((M - tempBudget) / (budgets.length - availableBudgetNum));
 
 		if (availableBudgetNum == budgets.length) {
 			Arrays.sort(budgets);
 			answer = budgets[budgets.length - 1];
 			return answer;
 		}
+		if (maxBudget < answer) {
+			answer = maxBudget;
+			return answer;
+		}
 
-		// answer = (int)((M - tempBudget) / (budgets.length - availableBudgetNum));
 
 		return answer;
     }
