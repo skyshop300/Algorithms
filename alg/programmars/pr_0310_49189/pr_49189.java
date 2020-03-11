06ccf393455563b07c1c5be4c163601076387851
skyshop300 <skyshop5210@gmail.com>
2020-03-11 23:09:12 +0900

    42578

--- alg/programmars/pr_0310_49189/pr_49189.java
+++ alg/programmars/pr_0310_49189/pr_49189.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 public class pr_49189 {
 	public static void main(String[] args) {
-		// String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
-		String[][] clothes = {{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}};
+		String[][] clothes = {{"yellow_hat", "headgear"}, {"blue_sunglasses", "eyewear"}, {"green_turban", "headgear"}};
+		// String[][] clothes = {{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}};
 		Solution sol = new Solution();
 		System.out.println(sol.solution(clothes));
 	}
@@ -13,21 +13,18 @@ public class pr_49189 {
 class Solution {
 	public int solution(String[][] clothes) {
 		Map<String, Integer> map = new HashMap<>();
-		int sum = 0;
-		int mul = 1;
+		int answer = 1;
 
 		for(int i = 0; i < clothes.length; i++) { 
 			map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
 		}
 
 		for(int value : map.values()) {
-			System.out.println("value:" + value);
-			sum += value;
-			mul *= value;
+			System.out.println("value: " + value);
+			answer *= value+1;
 		}
-		int answer = sum + mul;
 
-		return answer;
+		return answer-1;
 	}
 
 }
